aa3f053810430ff1d0097d87a2785895aa4ee191
Mansi Patel <patelmansi02072006@gmail.com>
2026-09-01 13:26:24 +0530

    Time: 8 ms (46.95%), Space: 46.8 MB (85.28%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,34 +1,29 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-
-        // Arrays.sort(nums);
-        // int l = 0;
-        // int r = nums.length - 1;
-
-        // while(l < r) {
-        //     int sum = nums[l] + nums[r];
-        //     if(sum == target) {
-        //         return new int[]{l, r};
-        //     }
-        //     else if(sum < target) {
-        //         l++;
-        //     }
-        //     else {
-        //         r--;
-        //     }
-        // }
-        // return new int[]{};
-
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for(int i=0;i<nums.length;i++){
-            int temp = target - nums[i];
-
-            if(map.containsKey(temp)){
-                return new int[]{map.get(temp),i};
-            }
-            map.put(nums[i], i);
+        int[][] arr = new int[nums.length][2];
+        for(int i = 0; i < nums.length; i++) {
+            arr[i][0] = nums[i]; 
+            arr[i][1] = i;       
         }
+
+        Arrays.sort(arr, (a, b) -> a[0] - b[0]);
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while(left < right) {
+            int sum = arr[left][0] + arr[right][0];
+            if(sum == target) {
+                return new int[]{arr[left][1], arr[right][1]};
+            }
+            else if(sum < target) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+
         return new int[]{};
     }
 }
