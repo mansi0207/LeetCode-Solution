64b4f8b5fb5711e38516e1f07469754983de0102
Mansi Patel <patelmansi02072006@gmail.com>
2026-09-02 10:36:43 +0530

    Time: 20 ms (66.33%), Space: 45.7 MB (63.04%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,27 +1,28 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-
         Arrays.sort(nums);
 
         List<List<Integer>> ans = new ArrayList<>();
 
-        for(int i=0;i<nums.length;i++){
-            if(i > 0 && nums[i] == nums[i -1]){
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            if(i > 0 && nums[i] == nums[i-1]){
                 continue;
             }
-            for(int j = i+1; j< nums.length;j++){
-                if(j > i+1 && nums[j] == nums[j -1]){
+
+            for(int j=i+1;j<n;j++){
+                if(j > i+1 && nums[j] == nums[j - 1]){
                     continue;
                 }
 
-                int left = j + 1;
-                int right = nums.length - 1;
+                int left = j+1;
+                int right = n - 1;
 
                 while(left < right){
-                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+                    long sum = (long) nums[i]+nums[j]+nums[left]+nums[right];
 
                     if(sum == target){
-                        ans.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        ans.add(Arrays.asList(nums[i],nums[j],nums[left],nums[right]));
                         left++;
                         right--;
 
