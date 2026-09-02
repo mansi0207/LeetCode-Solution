42ec5e6314231dd13dd959a2624da14aa8a754d0
Mansi Patel <patelmansi02072006@gmail.com>
2026-09-02 08:42:42 +0530

    Time: 11 ms (71.93%), Space: 47 MB (24.16%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.java
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.java
@@ -1,37 +1,43 @@
 class Solution {
     public int findPairs(int[] nums, int k) {
+
         Arrays.sort(nums);
-
-        int i = 0;
-        int j = 1;
         int count = 0;
-
-        while(j < nums.length){
-            if(i==j){
-                j++;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
 
-            int diff = nums[j] - nums[i];
-            if(diff == k){
+            int target = nums[i] + k;
+
+            if (search(nums, i + 1, target)) {
                 count++;
-
-                int l = nums[i];
-                int r = nums[j];
-
-                while(i < nums.length && nums[i] == l){
-                    i++;
-                }
-                while(j < nums.length && nums[j] == r){
-                    j++;
-                }
-            }
-            else if(diff < k){
-                j++;
-            }else{
-                i++;
             }
         }
+
         return count;
+    }
+
+    public boolean search(int[] nums, int start, int target) {
+
+        int left = start;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return true;
+            }
+
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
     }
 }
